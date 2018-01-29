ed2743de1e154ebaa6de52e993eac2d9957c7487
hadar18-meet <hadarky@gmail.com>
2018-01-29 19:15:05 +0200

    secsesfull tamplates

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask
+from flask import render_template 
 app = Flask(__name__)
-@app.route(/singin.html)
+
+@app.route('/home')
+@app.route('/')
+def home():
+	return render_template('home.html')
+@app.route('/Q&A')
+def Q_and_A():
+	return render_template('Q&A.html')	
+@app.route('/profile')
+def profile():
+	return render_template('profile.html')	
+@app.route('/About')
+def About():
+	return render_template('About.html')	
+@app.route('/singin')
 def singin():
-	
-	pass
+	return render_template('singin.html')	
+
